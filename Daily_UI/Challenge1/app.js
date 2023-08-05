998761c8ceff6ee171e185f1c56401a69eb32077
Mustafa DUMAN <115396551+u-MuDum@users.noreply.github.com>
2023-08-05 14:18:59 +0300

    Challenge 1 Kullanıcı Kayıt Ekranı

--- Daily_UI/Challenge1/app.js
+++ Daily_UI/Challenge1/app.js
@@ -1,1 +1,24 @@
+const modal = document.getElementById("modal");
 
+function modalAc() {
+  modal.style.display = "block";
+}
+
+function modalKapat() {
+  modal.style.display = "none";
+}
+
+function formuGonder() {
+  const ad = document.getElementById("ad").value;
+  const email = document.getElementById("email").value;
+  const telefon = document.getElementById("telefon").value;
+  const uygunluk = document.getElementById("uygunluk").value;
+
+  // Form verileri burada işlenebilir, bir sunucuya gönderilebilir veya bir veritabanına kaydedilebilir.
+  console.log("Ad:", ad);
+  console.log("E-posta:", email);
+  console.log("Telefon:", telefon);
+  console.log("Uygunluk Durumu:", uygunluk);
+
+  modalKapat();
+}
